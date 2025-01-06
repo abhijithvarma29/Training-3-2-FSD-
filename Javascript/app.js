5ef5b591fbd6dd536af67d6d4075cdb391496f1e
abhijithvarma29 <abhijithvarma29@gmail.com>
2025-01-06 17:58:30 +0530

    Javascript codes

---
+++ Javascript/app.js
@@ -0,0 +1,50 @@
+// console.log("Js is linked to html");
+// document.write("Javascript Linked");
+
+// var user="SAM";
+// user="miller";//re-initialization
+// console.log(user);
+
+// let num=100;
+// num=2;
+// console.log(num);
+
+// const dob=2005;
+// //dob=2006; //error since value cannot be changed
+// console.log(dob);
+
+// var a=10;
+// var a=30; //re-declaration
+// console.log(a);
+
+// let b=20;
+// let b=50;
+// console.log(b);
+
+// const c=true;
+// const c=false;
+// console.log(c);
+
+let str1="Double quotes";
+console.log(str1);
+console.log(typeof(str1));
+
+let str2='single quotes';
+console.log(str1);
+console.log(typeof(str1));
+
+let str3=`template literal`;
+console.log(str1);
+console.log(typeof(str1));
+
+let num=10.57
+console.log(num,typeof(num));
+
+let bool=true;
+console.log(bool,typeof(bool));
+
+let a;
+console.log(a,typeof(a));
+
+let b=null;
+console.log(b,typeof(b));

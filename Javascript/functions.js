b6314a9c84167065528fc6b42c610c5933e770c4
abhijithvarma29 <abhijithvarma29@gmail.com>
2025-01-08 11:51:52 +0530

    Javascript

---
+++ Javascript/functions.js
@@ -0,0 +1,21 @@
+// //Without parameters and arguments
+// function demo(){//function declaration
+//     console.log("Hello i am function");
+// }
+// demo(); //function calling
+//demo();
+
+// //With parameters and arguments
+// function add(a, b){
+//     // console.log("a=" + a);
+//     // console.log("b=" + b);
+//     console.log("a + b = "+(a+b));
+// }
+// add(10,20);
+// add(5,6);
+
+function addtocart(){
+    document.body.style.background="teal";
+    alert("Hello");
+}
+addtocart();
---
+++ Javascript/loopingstatements.js
@@ -0,0 +1,34 @@
+//
+
+// let n=100;
+// for(let i=1;i<=n;i++){
+//     if(i%2==0){
+//         console.log(i+" is even number");
+//     }else{
+//         console.log(i+" is odd number");
+//     }
+// }
+
+//write a js program to find factorial of a number
+//5!= 5*4*3*2*1
+// let num=Number(prompt("Enter a number"));
+// let fact=1;
+// for(let i=1;i<=num;i++){
+// // for(let i=num;i>=1;i--){
+//     fact*=i;
+// }
+// console.log("Factorial of "+num+" : "+fact);
+
+//WHILE LOOP
+// let i=1;
+// while(i<=5){
+//     console.log("Good Morning!")
+//     i++; 
+// }
+
+//DO WHILE
+let i=1;
+do{
+    console.log("please wake up");
+    i++; //infinite loop if not included
+}while(i<=5);

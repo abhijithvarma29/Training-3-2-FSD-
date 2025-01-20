44ea46abebee3073d2062ee210edd31c4924e258
abhijithvarma29 <abhijithvarma29@gmail.com>
2025-01-20 10:27:00 +0530

    Javascript

---
+++ Javascript/webstorage.js
@@ -0,0 +1,27 @@
+
+// localStorage.setItem("user1","abhi");
+// localStorage.setItem("user2","pavan");
+// localStorage.setItem("user3","Ritwik");
+// localStorage.setItem("user4","Mani");
+// localStorage.setItem("user5","Aditya");
+
+// let user5=localStorage.getItem("user5");
+// console.log(user5);
+
+// localStorage.removeItem("user2");
+
+// localStorage.clear();
+
+
+sessionStorage.setItem("user1","abhi");
+sessionStorage.setItem("user2","pavan");
+sessionStorage.setItem("user3","Ritwik");
+sessionStorage.setItem("user4","Mani");
+sessionStorage.setItem("user5","Aditya");
+
+let user5=sessionStorage.getItem("user5");
+console.log(user5);
+
+sessionStorage.removeItem("user2");
+
+// sessionStorage.clear();

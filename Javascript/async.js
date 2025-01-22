40a23934999a9dbf165737b7cecf5f0569ac53ef
abhijithvarma29 <abhijithvarma29@gmail.com>
2025-01-22 10:26:12 +0530

    javascript

--- Javascript/async.js
+++ Javascript/async.js
@@ -23,10 +23,39 @@
 //     console.log(error);
 // }).finally(()=>console.log("Finally"));
 
-let p3=new Promise((resolve,reject)=>{
-    reject("Failure");
-});
-// console.log(p3);
-p3.then((data)=>{console.log(data);})
-.catch((error)=>{console.log(error)})
-.finally(()=>console.log("Finally"));
+// let p3=new Promise((resolve,reject)=>{
+//     reject("Failure");
+// });
+// // console.log(p3);
+// p3.then((data)=>{console.log(data);})
+// .catch((error)=>{console.log(error)})
+// .finally(()=>console.log("Finally"));
+
+//API FETCHING
+// console.log(window);
+function fetchusers(){
+    let res=window.fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(res);
+    res.then((response)=>{
+        // console.log(response);
+        // console.log(response.json());
+        return response.json().then(data=>{
+            // console.log(data);
+            let store = document.getElementById("store");
+            // console.log(store);
+            data.map((user)=>{
+                // console.log(user);
+                store.innerHTML+=`
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+                `
+            })
+        })
+    })
+    .catch((err)=>{console.log(err)});
+}
+fetchusers();

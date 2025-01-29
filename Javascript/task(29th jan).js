ac16fcd18e2a9c18d6dc6c7d0b7117d6d13d3d9c
abhijithvarma29 <abhijithvarma29@gmail.com>
2025-01-29 20:13:01 +0530

    Javascript training

---
+++ Javascript/task(29th jan).js
@@ -0,0 +1,47 @@
+//? take array of objects, each object contains empid, empname, empcompany, empsalary and empaddress where empaddress is again an object which contains empcity,emparea. print all those values on the ui in the form of table
+
+let employees =[
+    {
+        empid: 1,
+        empname: "Ravi",
+        empcompany: "ABC",
+        empsalary: 50000,
+        empaddress: {
+            empcity: "Hyderabad",
+            emparea: "Kompally"
+        }
+    },
+    {
+        empid: 2,
+        empname: "Raj",
+        empcompany: "XYZ",
+        empsalary: 60000,
+        empaddress: {
+            empcity: "Hyderabad",
+            emparea: "miyapur"
+        }
+    },
+    {
+        empid: 3,
+        empname: "Rohan",
+        empcompany: "XYZ",
+        empsalary: 70000,
+        empaddress: {
+            empcity: "Hyderabad",
+            emparea: "Kompally"
+        }
+    }
+]
+const table = document.getElementById("table");
+employees.forEach(emp => {
+const row = document.createElement("tr");
+row.innerHTML = `
+    <td>${emp.empid}</td>
+    <td>${emp.empname}</td>
+    <td>${emp.empcompany}</td>
+    <td>${emp.empsalary}</td>
+    <td>${emp.empaddress.empcity}</td>
+    <td>${emp.empaddress.emparea}</td>
+    `;
+    table.appendChild(row);
+});
